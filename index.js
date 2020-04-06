4b82d9225e606d4e6a922f41c37f2f0a8fee4af5
LoneRifle <LoneRifle@users.noreply.github.com>
2020-04-06 09:48:44 +0800

    Tweak comma-dangle for functions
    Ensure that functions are allowed to not have a dangling comma for the last line in a 
    multiline declaration or call

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ module.exports = {
   },
   "rules": {
     "no-console": ["error", {"allow": ["warn", "error"]}],
-    "comma-dangle": ["error", "always-multiline"],
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "only-multiline"
+    }],
     "newline-per-chained-call": ["error"],
   }
 }
