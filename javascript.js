df11d4a0e07b9a6cd18ba4169e87c9a714e38b36
Blake Gong <blake@open.gov.sg>
2023-02-25 11:08:56 +0800

    chore: restart again, with extensibility in mind

---
+++ javascript.js
@@ -0,0 +1,33 @@
+/**
+ * Base JavaScript config does 2 things:
+ * - Apply basic recommended rule sets
+ * - Fix imports
+ */
+module.exports = {
+  root: true,
+  plugins: ['import', 'prettier', 'simple-import-sort'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2022: true,
+    jest: true,
+    node: true,
+  },
+  // https://github.com/eslint/espree#options
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // simple-import-sort does not have a preset. Below is the default from
+    // https://github.com/lydell/eslint-plugin-simple-import-sort#usage
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+  },
+}
